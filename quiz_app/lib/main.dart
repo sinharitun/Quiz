373ef7a7b9194cbf1110f4238f39da556985c9a3
Sinharitun <ritunrajoff230301@gmail.com>
2024-08-11 23:21:45 +0530

    first change

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png'),
+            
+
+          ],
+        ),
+      ),
+    ),
+  ));
+}
