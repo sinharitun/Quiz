4b0f8caece5eee8700bb85e54e05fd2ce57cdce3
Sinharitun <ritunrajoff230301@gmail.com>
2024-08-19 23:50:18 +0530

    add question

--- quiz_app/lib/quiz_test.dart
+++ quiz_app/lib/quiz_test.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
 
-class QuizTest extends StatefulWidget{
-
+class QuizTest extends StatefulWidget {
   const QuizTest({super.key});
   @override
   State<StatefulWidget> createState() {
-   return _QuizTestState();
+    return _QuizTestState();
   }
 }
 
-class _QuizTestState extends State<QuizTest>
-{
+class _QuizTestState extends State<QuizTest> {
   @override
   Widget build(BuildContext context) {
-    return const Text('hii');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("question",style: TextStyle(color: Colors.white,fontSize: 28),),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('answer1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('answer2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('answer3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('answer4'),
+          ),
+      
+        ],
+      ),
+    );
   }
 }
